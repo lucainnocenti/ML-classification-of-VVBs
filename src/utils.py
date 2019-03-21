36896d2c2ef0b8f994c36c283eb46cd11292ecb5
Luca Innocenti <lukeinnocenti@gmail.com>
2019-03-21 21:43:28 +0000

    src files updated

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ def abs2(data):
     return np.abs(data)**2
 
 
-def borderless_imshow_save(data, outputname, size=1, dpi=80):
+def borderless_imshow_save(data, outputname, size=1, dpi=80, imshow_opts={}):
     width = 1 * size
     height = data.shape[0] / data.shape[1] * size
     size = (width, height)
@@ -20,7 +20,7 @@ def borderless_imshow_save(data, outputname, size=1, dpi=80):
     ax.set_axis_off()
     fig.add_axes(ax)
 
-    ax.imshow(data)
+    ax.imshow(data, **imshow_opts)
     plt.savefig(outputname, dpi=dpi)
     plt.close(fig)
 
@@ -34,9 +34,36 @@ def imshow_intensities(amplitudes, imshow_opts={}, ax=None):
     ax.axis('off')
 
 
+def imshow_stokes_probs(prob_vectors, imshow_opts={}, axs=None):
+    if axs is None:
+        _, axs = plt.subplots(1, ncols=3)
+    labels = ['0/1', '+/-', 'L/R']
+    for ax, prob_vector, label in zip(axs, prob_vectors, labels):
+        ax.imshow(prob_vector, **imshow_opts)
+        ax.set_title(label)
+
+
 def add_noise_to_array(data, noise_level=0.1):
     """Add white noise to the data."""
     data = np.asarray(data)
     range_ = data.max() - data.min()
     return data + np.random.randn(*data.shape) * (noise_level * range_)
 
+
+def make_into_rgb_format(array):
+    """Make an N x M x 3 array fit for the RGB format."""
+    # rescale to that min and max are 0 and 255, respectively
+    # NOTE: I am not so sure about the correctness of the rescaling, CHECK
+    array = array + np.abs(array.min())
+    array = (array * 255 / array.max()).astype(np.uint8)
+    return array
+
+
+def merge_dict_elements(dict_):
+    data = None
+    for key in list(dict_):
+        if data is None:
+            data = dict_[key]
+        else:
+            data = np.append(data, dict_[key], axis=0)
+    return data
--- src/vector_vortex_beams.py
+++ src/vector_vortex_beams.py
@@ -2,23 +2,26 @@ import math
 import os
 import sys
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy
 import scipy.special
-import seaborn as sns
-from PIL import Image
 
 import keras
+import matplotlib.pyplot as plt
+import seaborn as sns
 import tensorflow
+from PIL import Image
+
+from utils import abs2
+
 
 
 def angle(x, y):
     return np.arctan2(x, y)
 
 
-def LaguerreGauss(x, y, p, m, w0=1.):
+def LaguerreGauss(x, y, p, m, w0):
     laguerreP = scipy.special.genlaguerre(p, np.abs(m))
     R2 = x**2 + y**2
     lg = 0 * 1j + np.sqrt((2**(np.abs(m) + 1) * scipy.special.gamma(1 + p)) /
@@ -28,6 +31,98 @@ def LaguerreGauss(x, y, p, m, w0=1.):
     # additional phase depending on `m` dof
     lg *= np.exp(- 1j * m * angle(x, y))
     return lg
+
+
+def _bloch_sphere_vector(theta, phi):
+    """Amplitudes of Bloch sphere vector in usual notation."""
+    return np.asarray([
+        np.cos(theta / 2),
+        np.sin(theta / 2) * np.exp(1j * phi)
+    ])
+
+
+def _su2_basis_states(which):
+    """Return commonly used basis states from a name."""
+    if not isinstance(which, (list, tuple)):
+        which = [which]
+    answers = []
+    for a_which in which:
+        if a_which == 'H' or a_which == '0':
+            answers.append(np.array([1, 0]))
+        elif a_which == 'V' or a_which == '1':
+            answers.append(np.array([0, 1]))
+        elif a_which == 'L':
+            answers.append([1, -1j] / np.sqrt(2))
+        elif a_which == 'R':
+            answers.append([1, 1j] / np.sqrt(2))
+        elif a_which == 'D' or a_which == '+':
+            answers.append([1, 1] / np.sqrt(2))
+        elif a_which == 'A' or a_which == '-':
+            answers.append([1, -1] / np.sqrt(2))
+    return answers
+
+
+def hyperentangled_qubit_projection(qubit_amps, qudits_amps, projectors):
+    """Project the qubit dof in the hyperentangled state.
+    
+    Attributes
+    ----------
+    qubit_amps : pair of complex numbers
+        The complex coefficients of the qubit state.
+    qudits_amps : pair of lists
+        The qudit states associated with the qubit states. The full state is
+        assumed to be
+            qubit_amps[0] |0, qudits_amps[0]> +
+            qubit_amps[1] |1, qudits_amps[1]>.
+    projectors : list of pairs of complex numbers
+        Each element should be a pair representing the qubit state that we
+        want the overall state to be projected on.
+
+    """
+    probs = []
+    for projector_amps in projectors:
+        probs.append(abs2(
+            np.conjugate(projector_amps[0]) * qubit_amps[0] * qudits_amps[0] +
+            np.conjugate(projector_amps[1]) * qubit_amps[1] * qudits_amps[1]
+        ))
+    return probs
+
+
+def probabilities_to_stokes_parameters(list_of_probabilities):
+    """Compute stokes parameters from a set of six probability vectors."""
+    p0, p1, p_plus, p_minus, p_L, p_R = list_of_probabilities
+    return np.asarray([p0 - p1, p_plus - p_minus, p_R - p_L])
+
+
+def vector_vortex_stokes_pars(X, Y, p, m_pair, w0, polarization_state):
+    """Return the Stokes probability vectors for a given vector vortex beam.
+    
+    Attributes
+    ----------
+    m_pair : tuple of ints
+        A vector vortex beam is a superposition c0 |m1> + c1 |m2>, and `m_pair`
+        contains m1 and m2.
+    polarization_state : pair of floats
+        Coefficients of the polarization state that is entangled with the OAM.
+        We assume the polarization state is pure, and the parameters are the
+        complex coefficient in the computational basis.
+    
+    Returns
+    -------
+    The set of three Stokes probability vectors associated with the state.
+    """
+    if len(m_pair) != 2:
+        raise ValueError('There must be two elements in `m_pair`.')
+    amps_first_m = LaguerreGauss(X, Y, p, m_pair[0], w0)
+    amps_second_m = LaguerreGauss(X, Y, p, m_pair[1], w0)
+
+    states_to_project_upon = _su2_basis_states(['0', '1', '+', '-', 'R', 'L'])
+    probabilities_for_basis_states = hyperentangled_qubit_projection(
+        qubit_amps=polarization_state,
+        qudits_amps=[amps_first_m, amps_second_m],
+        projectors=states_to_project_upon
+    )
+    return probabilities_to_stokes_parameters(probabilities_for_basis_states)
 
 
 def rotation_matrix(theta):
@@ -67,25 +162,4 @@ def polarization_projection_matrix_from_waveplates(alpha_HWP, alpha_QWP):
     """Compute polarization projectors with rotated waveplates matrices.
     """
     return np.dot(rotated_HWP_matrix(alpha_HWP), rotated_QWP_matrix(alpha_QWP))
-
-
-def su2_basis_states(which):
-    """Return commonly used basis states from a name."""
-    if which == 'H':
-        return np.array([1, 0])
-    elif which == 'V':
-        return np.array([0, 1])
-    elif which == 'L':
-        return [1, -1j] / np.sqrt(2)
-    elif which == 'R':
-        return [1, 1j] / np.sqrt(2)
-    elif which == 'D':
-        return [1, 1] / np.sqrt(2)
-    elif which == 'A':
-        return [1, -1] / np.sqrt(2)
-
-
-def bloch_sphere_vector(theta, phi):
-    """Amplitudes of Bloch sphere vector in usual notation."""
-    return np.asarray([np.cos(theta / 2), np.sin(theta / 2) * np.exp(1j * phi)])
 
